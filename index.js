9116c03ea55fd14b6a32b0fc155a10a188b93b2d
Leandro Galdino de Oliveira <leandro.galdino.oliveira@gmail.com>
2026-07-28 14:15:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,77 +1,16 @@
 require('dotenv').config();
+const express = require('express');
 
-const mongoose = require('mongoose');
-const conectaNaDatabase = require('./src/config/dbConnect');
-const {iniciarSerial, encerrarSerial} = require('./src/serial/serial');
 
-const app = require('./src/express/routers')
+const {app, iniciarAplicacao, encerrarAplicacao} = require('./src/express/routers');
 const portExpress = 3000;
 
 app.set('view engine', 'ejs');
 app.set ('views', './src/views')
 
-async function iniciarAplicacao() {
-  try {
-   
-    await conectaNaDatabase();
-
-    app.listen(portExpress, () => {
-        console.log(
-        `Servidor Express funcionando em http://localhost:${portExpress}`
-      );
-    });
-
-    iniciarSerial();
-
-  } catch (erro) {
-    console.error(
-      'Nao foi posssivel iniciar a aplicacao:',
-      erro.message
-    );
-
-    
-  }
-}
-
-async function encerrarAplicacao() {
-  try {
-    await encerrarSerial();
-    await mongoose.disconnect();
-
-    console.log('Aplicacao encerrada.');
-    process.exit(0);
-    
-  } catch (erro) {
-    console.error(
-      'Erro ao encerrar a aplicacao:',
-      erro.message
-    );
-
-    process.exit(1);
-  }
-}
-
-process.on('SIGINT', encerrarAplicacao);
-
+ 
 iniciarAplicacao();
-async function encerrarAplicacao() {
-  try {
-    await encerrarSerial();
-    await mongoose.disconnect();
 
-    console.log('Aplicação encerrada.');
-    process.exit(0);
-    
-  } catch (erro) {
-    console.error(
-      'Erro ao encerrar a aplicação:',
-      erro.message
-    );
-
-    process.exit(1);
-  }
-}
-
-process.on('SIGINT', encerrarAplicacao);
-
-iniciarAplicacao();
+process.on('SIGINT', ()=> {
+     process.exit(0);
+});
